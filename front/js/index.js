160b888972d903659adde7fcce654a9fe3b6c3c9
fausrguez <faustinorodriguezfleitas@gmail.com>
2018-03-23 20:54:07 +0000

    something

--- front/js/index.js
+++ front/js/index.js
@@ -1,41 +1,45 @@
-
-
-var id_owner = sessionStorage.setItem('ownerId' , Math.random().toString(36).substr(7));
-var id_receiver = sessionStorage.setItem('receiverId' , Math.random().toString(36).substr(7));
+var id_owner = sessionStorage.setItem('ownerId', Math.random().toString(36).substr(7));
+var id_receiver = sessionStorage.setItem('receiverId', Math.random().toString(36).substr(7));
 
 
 
 
 
 function messageToSend() {
-    
+
     var message = $('#textBox').val();
 
     var MESSAGE = {
-        owner : id_owner,
-        reciever: id_receiver,        
-        text : message 
+        owner: id_owner,
+        reciever: id_receiver,
+        text: message
     }
 
-    $.ajax(
-        {
+    $.ajax({
             type: 'POST',
             url: 'http://localhost:5000/users',
             data: JSON.stringify(MESSAGE),
             contentType: 'application/json',
             dataType: 'json'
-        }
-    )
-    .then(response => {
-        console.log(response);
+        })
+        .then(response => {
+            console.log(response);
 
-    })
-    .catch(error => {
-        console.log("Error, algo ha fallado")
-    })
+        })
+        .catch(error => {
+            console.log("Error, algo ha fallado")
+        })
 
 }
 
 function messageToRecieve() {
-
+    $.get('/')
+    $.('#messages').append(`
+    <div class="col s12 m6">
+    <div class="card blue-grey darken-1">
+        <div class="card-content white-text">
+            <p></p>
+        </div>
+    </div>
+</div>`)
 }
