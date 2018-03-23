352fcdb621760c56b19413e14d8189fa92f3bd42
unknown <yonaycl@gmail.com>
2018-03-23 20:49:22 +0000

    getConversation

--- back/api/messages/index.js
+++ back/api/messages/index.js
@@ -3,5 +3,6 @@ const messageController = require('./messageController')
 
 router.get('/',messageController.getAll);
 router.post('/',messageController.createMessage);
+router.get('/chat/',messageController.getConversation);
 
 module.exports = router
--- back/api/messages/messageController.js
+++ back/api/messages/messageController.js
@@ -1,8 +1,10 @@
-const messsage  = require('./messagesModel')
+const _ = require('lodash');
+const messsage = require('./messagesModel')
 
 module.exports = {
     getAll: getAll,
-    createMessage: createMessage
+    createMessage: createMessage,
+    getConversation: getConversation
 };
 
 function getAll(req, res) {
@@ -13,8 +15,23 @@ function getAll(req, res) {
 
 function createMessage(req, res) {
     var message = req.body;
-    message.timestap = Date.now();
+    message.createdAt = Date.now();
     return messsage.create(req.body)
         .then(response => res.json(response))
         .catch((err) => res.status(400).send());
+}
+
+function getConversation(req, res) {
+    var owner = req.query.owner;
+    var receiver = req.query.receiver;
+    var messagesOfUsers = [];
+    messsage.find({ owner: owner, receiver: receiver }, (error, response) => {
+        messagesOfUsers.push(response)
+        messsage.find({ owner: receiver, receiver: owner }, (error, response) => {
+            messagesOfUsers.push(response)
+            messagesOfUsers = _.flatten(messagesOfUsers);
+            messagesOfUsers = _.orderBy(messagesOfUsers,['createdAt'],['asc'])
+            res.json(messagesOfUsers)
+        })
+    })
 }
